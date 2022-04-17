18d6ebf7bf36f42bf3ace3ce61259d95bf15da02
toufanialfarisi <toufani.deeplearning@gmail.com>
2022-04-17 17:09:51 +0700

    update base url structur

--- src/api/index.js
+++ src/api/index.js
@@ -1,15 +1,15 @@
 import axios from "axios";
-import { FETCH_SUCCESS, GET_ALL_POST } from "../constant";
+import { FETCH_SUCCESS, GET_ALL_POST, BASE_URL } from "../constant";
 
 const getData = async (dispatch, id) => {
   axios
-    .get(`https://jsonplaceholder.typicode.com/todos/${id}`)
+    .get(`${BASE_URL}/${id}`)
     .then((res) => dispatch({ type: FETCH_SUCCESS, payload: res.data }));
 };
 
 const getAllData = async (dispatch) => {
   axios
-    .get("https://jsonplaceholder.typicode.com/todos")
+    .get(`${BASE_URL}`)
     .then((res) => dispatch({ type: GET_ALL_POST, payload: res.data }));
 };
 
--- src/constant/index.js
+++ src/constant/index.js
@@ -6,6 +6,7 @@ const RESET = "RESET";
 const FETCH_SUCCESS = "FETCH_SUCCESS";
 const FETCH_FAILED = "FETCH_FAILED";
 const GET_ALL_POST = "GET_ALL_POST";
+const BASE_URL = "https://jsonplaceholder.typicode.com/todos";
 
 export {
   INCREMENT,
@@ -16,4 +17,5 @@ export {
   FETCH_SUCCESS,
   FETCH_FAILED,
   GET_ALL_POST,
+  BASE_URL,
 };
